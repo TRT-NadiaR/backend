d36c270c08bd5587c8eca1aae6115bc704b954c4
TRT-NadiaR <nadia.rashad@raytheon.co.uk>
2020-11-04 09:06:54 +0000

    got the post request working again, working on get request as data is not being passed from front end

--- src/nurseryUsers.js
+++ src/nurseryUsers.js
@@ -4,7 +4,30 @@ function createRouter(db) {
   const router = express.Router();
   const username = '';
 
+
+  router.get('/nurseryUserData/logging-in', function (req, res, next) {
+    console.log('in back end get request')
+    console.log(req.body, 'username')
+    //console.log(req.body.password, 'password')
+    // db.query(
+    //   'SELECT id, adult_first_name, child_first_name FROM nurseryUserData WHERE adult_first_name=? ',
+    //   [console.log('hellooooo'),  10*(req.params.page || 0)],
+    //   (error, results) => {
+    //     if (error) {
+    //       console.log(error, 'error');
+    //       res.status(500).json({status: 'error'});
+    //     } else {
+    //       res.status(200).json(results, 'results');
+    //     }
+    //   }
+    // );
+  });
+
+
+
+
   router.post('/nurseryUserData', (req, res, next) => {
+    console.log(req.body)
     db.query(
       'INSERT INTO nurseryUserData (username, email, password, adult_first_name, adult_last_name, child_first_name, child_last_name, phoneHome, phoneMobile ) VALUES (?,?,?,?,?,?,?,?,?)',
       [req.body.username, req.body.email, req.body.password, req.body.name, req.body.parentLastName, req.body.childName, req.body.childLastName, req.body.phoneHome, req.body.phoneMob],
@@ -21,8 +44,8 @@ function createRouter(db) {
 
   router.get('/nurseryUserData', function (req, res, next) {
     db.query(
-      'SELECT id, adult_first_name, child_first_name FROM nurseryUserData WHERE adult_first_name=? ORDER BY date LIMIT 10 OFFSET ?',
-      [adult_first_name, 10*(req.params.page || 0)],
+      'SELECT id, adult_first_name, child_first_name FROM nurseryUserData WHERE adult_first_name=? ',
+      [req.body.adult_first_name, 10*(req.params.page || 0)],
       (error, results) => {
         if (error) {
           console.log(error);
