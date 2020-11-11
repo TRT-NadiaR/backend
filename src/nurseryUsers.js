872b6ea42d2e56f7c64fae5155e352c8b9b12b50
TRT-NadiaR <nadia.rashad@raytheon.co.uk>
2020-11-11 13:50:54 +0000

    commented out queries that are not getting used

--- src/nurseryUsers.js
+++ src/nurseryUsers.js
@@ -6,21 +6,22 @@ function createRouter(db) {
 
 
   router.get('/nurseryUserData/logging-in', function (req, res, next) {
-    console.log('in back end get request')
-    console.log(req.body, 'username')
-    //console.log(req.body.password, 'password')
-    // db.query(
-    //   'SELECT id, adult_first_name, child_first_name FROM nurseryUserData WHERE adult_first_name=? ',
-    //   [console.log('hellooooo'),  10*(req.params.page || 0)],
-    //   (error, results) => {
-    //     if (error) {
-    //       console.log(error, 'error');
-    //       res.status(500).json({status: 'error'});
-    //     } else {
-    //       res.status(200).json(results, 'results');
-    //     }
-    //   }
-    // );
+    console.log('in back end get requesttttttttttttttttttttttttttttttttt')
+    console.log(req.query, 'query')
+    
+    db.query(
+      'SELECT adult_first_name,adult_last_name, child_first_name,child_last_name, phoneMobile, phoneHome  FROM nurseryUserData WHERE username=? ',
+      [req.query.username,  10*(req.params.page || 0)],
+      (error, results) => {
+        if (error) {
+          console.log(error, 'error');
+          res.status(500).json({status: 'error'});
+        } else {
+          console.log('results are: ',results)
+          res.status(200).json({status: results[0]});
+        }
+      }
+    );
   });
 
 
@@ -42,48 +43,48 @@ function createRouter(db) {
     );
   });
 
-  router.get('/nurseryUserData', function (req, res, next) {
-    db.query(
-      'SELECT id, adult_first_name, child_first_name FROM nurseryUserData WHERE adult_first_name=? ',
-      [req.body.adult_first_name, 10*(req.params.page || 0)],
-      (error, results) => {
-        if (error) {
-          console.log(error);
-          res.status(500).json({status: 'error'});
-        } else {
-          res.status(200).json(results);
-        }
-      }
-    );
-  });
+  // router.get('/nurseryUserData', function (req, res, next) {
+  //   db.query(
+  //     'SELECT id, adult_first_name, child_first_name FROM nurseryUserData WHERE adult_first_name=? ',
+  //     [req.body.adult_first_name, 10*(req.params.page || 0)],
+  //     (error, results) => {
+  //       if (error) {
+  //         console.log(error);
+  //         res.status(500).json({status: 'error'});
+  //       } else {
+  //         res.status(200).json(results);
+  //       }
+  //     }
+  //   );
+  // });
 
-  router.put('/nurseryUserData/:id', function (req, res, next) {
-    db.query(
-      'UPDATE nurseryUserData SET username=?, phoneMobile=?, date=? WHERE id=? AND username=?',
-      [req.body.username, req.body.phoneMobile, new Date(req.body.date), req.params.id, username],
-      (error) => {
-        if (error) {
-          res.status(500).json({status: 'error'});
-        } else {
-          res.status(200).json({status: 'ok'});
-        }
-      }
-    );
-  });
+  // router.put('/nurseryUserData/:id', function (req, res, next) {
+  //   db.query(
+  //     'UPDATE nurseryUserData SET username=?, phoneMobile=?, date=? WHERE id=? AND username=?',
+  //     [req.body.username, req.body.phoneMobile, new Date(req.body.date), req.params.id, username],
+  //     (error) => {
+  //       if (error) {
+  //         res.status(500).json({status: 'error'});
+  //       } else {
+  //         res.status(200).json({status: 'ok'});
+  //       }
+  //     }
+  //   );
+  // });
 
-  router.delete('/nurseryUserData/:id', function (req, res, next) {
-    db.query(
-      'DELETE FROM nurseryUserData WHERE id=? AND username=?',
-      [req.params.id, username],
-      (error) => {
-        if (error) {
-          res.status(500).json({status: 'error'});
-        } else {
-          res.status(200).json({status: 'ok'});
-        }
-      }
-    );
-  });
+  // router.delete('/nurseryUserData/:id', function (req, res, next) {
+  //   db.query(
+  //     'DELETE FROM nurseryUserData WHERE id=? AND username=?',
+  //     [req.params.id, username],
+  //     (error) => {
+  //       if (error) {
+  //         res.status(500).json({status: 'error'});
+  //       } else {
+  //         res.status(200).json({status: 'ok'});
+  //       }
+  //     }
+  //   );
+  // });
 
   return router;
 }
